99332c0854d37c6a90e18da95398baba05fa10cf
milosdjurica <milosdjurica13@gmail.com>
2024-04-23 16:56:26 +0200

    type for CdpResponse and converting ilk to string

--- src/components/choose-token.tsx
+++ src/components/choose-token.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { vaultAbi } from "@/abi/vault.abi";
 import { vaultAddress } from "@/utils/consts";
-import { TokenType } from "@/utils/types";
+import { CdpResponse, TokenType } from "@/utils/types";
 import { TokenState, zustandStore } from "@/utils/zustandStore";
 import { useState } from "react";
 
@@ -12,15 +12,20 @@ export default function ChooseToken() {
   ]);
 
   const [cdpId, setCdpId] = useState(0);
+  const [cdpInfo, setCdpInfo] = useState<CdpResponse>();
 
+  // TODO call multiple times until gets 20 closest
   async function fetchCDP() {
     try {
       const vault = new window.web3.eth.Contract(vaultAbi, vaultAddress);
       // TODO -> Create types
-      const info = await vault.methods.getCdpInfo(cdpId).call();
-      const ilk = window.web3.utils.hexToAscii(info.ilk);
-      console.log("ilk", ilk);
+      let info: CdpResponse = await vault.methods.getCdpInfo(cdpId).call();
+      info.ilk = window.web3.utils.hexToAscii(info.ilk).replace(/\0/g, "");
+      console.log("ilk", info.ilk);
+      // TODO -> check if type is same as TOKEN, if yes then put it into state and print out
+      if (info.ilk === "ETH-C") setCdpInfo(info);
       console.log(info);
+      console.log("cdpInfo", cdpInfo);
     } catch (error) {
       console.log("error", error);
       throw error;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,12 @@
+import { Address, Bytes } from "web3";
+
 export type TokenType = "ETH" | "WBTC" | "WSTETH";
+
+export type CdpResponse = {
+  urn: Address;
+  owner: Address;
+  userAddr: Address;
+  ilk: Bytes;
+  collateral: BigInt;
+  debt: BigInt;
+};
