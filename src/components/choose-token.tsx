7407f71fb5825a6179f06162a9c603abc5aa8793
milosdjurica <milosdjurica13@gmail.com>
2024-04-24 21:24:06 +0200

    fetching 20 closest

--- src/components/choose-token.tsx
+++ src/components/choose-token.tsx
@@ -3,29 +3,19 @@ import { useEffect, useState } from "react";
 
 import { vaultAbi } from "@/utils/abi/vault.abi";
 import { vaultAddress } from "@/utils/consts";
-import {
-  CdpInfoStore,
-  CdpResponse,
-  TokenStore,
-  TokenType,
-} from "@/utils/types";
-import { cdpInfoArrayStore, tokenStore } from "@/utils/zustandStore";
+import { CdpResponse, TokenType } from "@/utils/types";
 
 export default function ChooseToken() {
-  const [token, setToken] = tokenStore((state: TokenStore) => [
-    state.token,
-    state.setToken,
-  ]);
-
-  const [cdpInfoArray, setCdpInfoArray] = cdpInfoArrayStore(
-    (state: CdpInfoStore) => [state.cdpInfoArray, state.setCdpInfoArray]
-  );
-
+  const [cdpInfoArray, setCdpInfoArray] = useState<CdpResponse[]>([]);
+  const [token, setToken] = useState<TokenType>("ETH");
   const [cdpId, setCdpId] = useState(0);
 
   useEffect(() => {
     const timerId = setTimeout(() => {
-      fetchCDP();
+      setCdpInfoArray([]);
+      if (cdpId > 0) {
+        fetchAll();
+      }
     }, 5000);
 
     return () => {
@@ -33,21 +23,37 @@ export default function ChooseToken() {
     };
   }, [cdpId, token]);
 
-  // TODO call multiple times until gets 20 closest
-  async function fetchCDP() {
+  async function fetchAll() {
+    let arr: CdpResponse[] = [];
+    let counter = 1;
+    const first = await fetchCDP(cdpId);
+    if (first) arr.push(first);
+    while (arr.length < 20) {
+      const foundHigher = await fetchCDP(cdpId + counter);
+      if (foundHigher) arr.push(foundHigher);
+      if (arr.length === 20) break;
+
+      const foundLower = await fetchCDP(cdpId - counter);
+      if (foundLower) arr.push(foundLower);
+      counter++;
+    }
+  }
+
+  async function fetchCDP(id: number) {
     try {
       const vault = new window.web3.eth.Contract(vaultAbi, vaultAddress);
-      let newCdpInfo: CdpResponse = await vault.methods
-        .getCdpInfo(cdpId)
-        .call();
-      newCdpInfo.id = cdpId;
+      let newCdpInfo: CdpResponse = await vault.methods.getCdpInfo(id).call();
+
+      newCdpInfo.id = id;
       // Transforming bytes into ETH/WBTC/WSTETH string
       newCdpInfo.ilk = window.web3.utils
         .hexToAscii(newCdpInfo.ilk) // "ETH-C\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000"
         .split("-")[0]; // "ETH"
 
-      if (newCdpInfo.ilk === token)
-        setCdpInfoArray([...cdpInfoArray, newCdpInfo]);
+      if (newCdpInfo.ilk === token) {
+        setCdpInfoArray((prev: CdpResponse[]) => [...prev, newCdpInfo]);
+        return newCdpInfo;
+      }
     } catch (error) {
       console.log("error", error);
       throw error;
@@ -77,6 +83,16 @@ export default function ChooseToken() {
           onChange={(e) => setCdpId(Number(e.target.value))}
         />
         <p>Selected CDP ID:{cdpId}</p>
+        {cdpInfoArray && (
+          <div>
+            {cdpInfoArray.map((cdpInfo, index) => (
+              <p key={index}>
+                ID : {cdpInfo.id} | VALUE : {Number(cdpInfo.collateral) / 1e18}{" "}
+                {cdpInfo.ilk}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
