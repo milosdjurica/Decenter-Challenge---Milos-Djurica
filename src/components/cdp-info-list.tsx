f0bc1fc4686f9df49a63095c1d3ae113f711e8e3
milosdjurica <milosdjurica13@gmail.com>
2024-04-26 21:35:11 +0200

    cdp info list table

--- src/components/cdp-info-list.tsx
+++ src/components/cdp-info-list.tsx
@@ -1,19 +1,52 @@
-import { CdpInfoFormatted } from "@/utils/types";
 import Link from "next/link";
+import { CdpInfoFormatted, TokenType } from "@/utils/types";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function CdpInfoList({
   cdpInfoArray,
+  token,
 }: {
   cdpInfoArray: CdpInfoFormatted[];
+  token: TokenType;
 }) {
   return (
     <div className="flex flex-col">
-      {cdpInfoArray.map((cdpInfo) => (
-        <Link href={"/" + cdpInfo.id} key={cdpInfo.id}>
-          ID : {cdpInfo.id} | COLLATERAL : {Number(cdpInfo.collateral)}{" "}
-          {cdpInfo.ilk} | DEBT : {Number(cdpInfo.debt)} DAI
-        </Link>
-      ))}
+      <Table className="md:w-[90%] m-auto border md:text-lg border-ring">
+        <TableCaption>
+          A list of 20 CDPs closest to the selected ID that have same collateral
+          type - {token}.
+        </TableCaption>
+
+        <TableHeader>
+          <TableRow className="border-b-primary">
+            <TableHead className="">CDP ID</TableHead>
+            <TableHead>Collateral in {token}</TableHead>
+            <TableHead>Debt in DAI</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {cdpInfoArray.map((cdpInfo) => (
+            <Link href={"/" + cdpInfo.id} key={cdpInfo.id} legacyBehavior>
+              <TableRow key={cdpInfo.id} className="hover:cursor-pointer">
+                <TableCell className="font-medium text-primary">
+                  {cdpInfo.id}
+                </TableCell>
+                <TableCell>{Number(cdpInfo.collateral)}</TableCell>
+                <TableCell>{Number(cdpInfo.debt)} DAI</TableCell>
+              </TableRow>
+            </Link>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
--- src/components/choose-token.tsx
+++ src/components/choose-token.tsx
@@ -139,7 +139,7 @@ export default function ChooseToken() {
           />
         </div>
       </div>
-      <CdpInfoList cdpInfoArray={cdpInfoArray} />
+      <CdpInfoList cdpInfoArray={cdpInfoArray} token={token} />
     </div>
   );
 }
