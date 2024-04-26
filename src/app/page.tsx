ba722934981fc95c8ed0ba92f88a716d5f7e2955
milosdjurica <milosdjurica13@gmail.com>
2024-04-26 19:18:45 +0200

    dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { initWeb3 } from "@/utils/web3";
 import { useEffect } from "react";
 import ChooseToken from "../components/choose-token";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   useEffect(() => {
@@ -11,9 +12,13 @@ export default function Home() {
 
   return (
     <div className="space-y-8">
-      <h1 className="text-center text-5xl font-semibold">
-        Decenter Challenge - Milos Djurica
-      </h1>
+      <div className="flex justify-between p-4 border-b-2 border-b-primary">
+        <h1 className="text-center text-5xl font-semibold">
+          <span className="text-primary">Decenter Challenge</span> - Milos
+          Djurica
+        </h1>
+        <ModeToggle />
+      </div>
       <ChooseToken />
     </div>
   );
